3312ccc8af6eb9404f4514c1581a92d2ef6de577
Александр Винокуров <alonzovink@gmail.com>
2017-09-18 14:30:34 +0300

    Create course.php

---
+++ course.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+ <head>
+  <meta charset="utf-8">
+  <title>Обращения к сервису Whois на PHP </title>
+  <link href="/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/css/font-awesome.min.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="shortcut icon" href="images/whois.png">
+  <style>
+   body {
+      background:url(images/bd.jpg)  no-repeat;
+    -moz-background-size: 100%; /* Firefox 3.6+ */
+    -webkit-background-size: 100%; /* Safari 3.1+ и Chrome 4.0+ */
+    -o-background-size: 100%; /* Opera 9.6+ */
+    background-size: 100%; /* Современные браузеры */
+   }
+  </style>
+ </head>
+ <body>
+<center><img src="images/whois.png" class="img-responsive"></center>
+ <div class="container">
+
+  
+  
+
+<center>
+ <form method="post">
+ <input type="text" class="form-control" name="ip" size="35">
+ <input type="submit" class="form-control" value="Введите IP-адрес" value="<?= htmlspecialchars($_REQUEST['ip']); ?>">
+ </form>
+</center>
+<?php
+if(!empty($_POST['ip'])) echo whois("whois.arin.net",$_POST['ip']);
+
+function whois($url,$ip)
+{
+  // Соединение с сокетом TCP, ожидающим на сервере "whois.arin.net" по 
+  // 43 порту. В результате возвращается дескриптор соединения $sock.
+  $sock = fsockopen($url, 43, $errno, $errstr);
+  if (!$sock) exit("$errno($errstr)");
+  else
+  {
+    echo $url."<br>";
+    // Записываем строку из переменной $_POST["ip"] в дескриптор сокета.
+    fputs ($sock, $ip."\r\n");
+    // Осуществляем чтение из дескриптора сокета.
+    $text = "";
+    while (!feof($sock))
+    {
+      $text .= fgets ($sock, 128)."<br>";
+    }
+    // закрываем соединение
+    fclose ($sock);
+
+    // Ищем реферальный сервере
+    $pattern = "|ReferralServer: whois://([^\n<:]+)|i";
+    preg_match($pattern, $text, $out);
+    if(!empty($out[1])) return whois($out[1], $ip);
+    else return $text;
+  }
+}
+?>
+</p>
+  <table width="20%" bordercolor="%">
+    <tr>
+      <td></td>
+    </tr>
+  </table>
+<br />
+<img src="/images/whois.png" class="img-responsive" style="max-width: 200px; width: 100%;"><h3>WHOIS</h3><p>WHOIS (от англ. who is — «кто это?») — сетевой протокол прикладного уровня, 
+  базирующийся на протоколе TCP (порт 43). 
+Основное применение — получение регистрационных данных о владельцах доменных имён, IP-адресов и автономных систем.</p><h3>Как это работает</h3><p>Мы обращаемся к сайту whois.arin.net и получаем по IP информацию <a href="https://github.com/SanyaFox/Personal-page">Исходный код страницы</a></p>
+
+
+<footer id="footer" class="footer"><!--Footer-->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <p class="pull-left">Copyright © <?php echo  date("Y"); ?></p>
+                <p class="pull-right">Александр Винокуров <a href="/">SanyaFox</a></p>
+            </div>
+        </div>
+    </div>
+</footer><!--/Footer-->
+</div>
+ </body>
+</html>
