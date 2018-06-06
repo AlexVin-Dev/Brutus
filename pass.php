6e426e5b62bb0ab2cf7e4679cbadd6fe3d156e30
Alexander Vinokourov <alonzovink@gmail.com>
2018-06-06 03:46:11 +0300

    Update pass.php

--- pass.php
+++ pass.php
@@ -2,23 +2,31 @@
  <head>
   <meta charset="utf-8">
   <title>Генератор паролей SanyaFox</title>
-	<link href="/css/bootstrap.min.css" rel="stylesheet">
-	<link href="/css/font-awesome.min.css" rel="stylesheet">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <link rel="shortcut icon" href="images/favicon/generate.png">
-  <style>
-   body {
-			background:url(images/bd.jpg)  no-repeat;
-    -moz-background-size: 100%; /* Firefox 3.6+ */
-    -webkit-background-size: 100%; /* Safari 3.1+ и Chrome 4.0+ */
-    -o-background-size: 100%; /* Opera 9.6+ */
-    background-size: 100%; /* Современные браузеры */
-   }
-  </style>
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
  </head>
  <body>
-<center><img src="images/safe-password-generator-logo.png" class="img-responsive"></center>
- <div class="container">
+<div class="table-container">
+    <div class="table-block footer-push">
+
+      <!-- Primary Page Layout
+           –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <div class="container">
+        <div class="row">
+          <div class="one-half column" style="margin-top: 25%">
 <form method=post>
 <div class="form-group">
 Генератор паролей
@@ -88,27 +96,27 @@
 
 ?>
 </strong></p>
-  <table width="20%" bordercolor="%">
-    <tr>
-      <td></td>
-    </tr>
-  </table>
+
 <br />
 <img src="/images/generate.png" class="img-responsive" style="max-width: 200px; width: 100%;"><h3>Надёжный Пароль</h3><p>Надёжный пароль - это ключ к защите вашей частной жизни и информации онлайн. 
 Использование разных паролей для разных сайтов - паролей длинных, с символами разных типов (цифрами, буквами, спец.символами) - поможет Вам защититься от попыток проникновения в ваши 
 аккаунты.</p><h3>Как это работает</h3><p>Пароль генерируется на Вашем компьютере, с помощью PHP. Эти пароли никогда не передаются в SanyaFox.ru. <a href="#">Исходный код страницы</a></p>
 <h3>Решение проблемы паролей</h3><p>Устали запоминать пароли? Менеджеры паролей, например SanyaFox, помогают Вам хранить пароли в одном безопасном месте, где они зашифрованы и доступны 
 только Вам. Встроенный генератор паролей также помогает Вам создавать надёжные пароли когда нужно. </p>
-
-<footer id="footer" class="footer navbar-fixed-bottom"><!--Footer-->
-    <div class="footer-bottom">
-        <div class="container">
-            <div class="row">
-                <p class="pull-left">Copyright © <?php echo  date("Y"); ?></p>
-                <p class="pull-right">Александр Винокуров SanyaFox</p>
-            </div>
+          </div>
         </div>
-    </div>
+      </div>  <!-- end primary div.container -->
+    </div> <!-- end primary div.table-block -->
+    <div class="table-block">
+      <!-- Page Footer Layout
+           –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <div class="container">
+        <footer id="footer" class="twelve columns">
+          Copyright © <?php echo  date("Y"); ?> | Александр Винокуров <a href="/">SanyaFox</a>
+        </footer>
+      </div> <!-- end footer div.container -->
+    </div>  <!-- end footer div.table-block -->
+  </div>
 </footer><!--/Footer-->
 </div>
  </body>
