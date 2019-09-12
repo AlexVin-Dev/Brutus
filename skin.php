517d925c668c14e81e8fb1d8cad2624f802fc98f
Alexander Vinokourov <alonzovink@gmail.com>
2019-09-12 22:09:12 +0300

    Update skin.php
    Исправлены ошибки
    Добавлена возможность получать с Ely.by UUID

--- skin.php
+++ skin.php
@@ -7,9 +7,12 @@ if(isset($_POST['username'])) {
 	$info = file_get_contents('http://skinsystem.ely.by/textures/'.$_POST['username'].'');
 	$info = json_decode($info, true);
 	$profiles = json_decode($profiles, true);
+
+
+  $uuid = preg_replace('#([a-z0-9]{8})([a-z0-9]{4})([a-z0-9]{4})([a-z0-9]{4})([a-z0-9]{12})#', '$1-$2-$3-$4-$5', $profiles['id']);
 }
 // Авторизация Debug
-$password = 'password';
+$password = '1111';
 ?>
   <!-- Basic Page Needs
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
@@ -17,7 +20,6 @@ $password = 'password';
   <title>Запрос  <?php if(isset($_POST['username'])) { echo "пользователя ".$profiles['name']; } else { echo "пользователя";} ?> -> Images</title>
   <meta name="description" content="">
   <meta name="author" content="">
-
   <!-- Mobile Specific Metas
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -33,6 +35,18 @@ $password = 'password';
   <!-- Favicon
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="icon" type="image/png" href="images/favicon.png">
+<style>
+   a.rollover {
+    background: url("<?php echo "http://ely.by/services/skins-renderer?url=".$info['SKIN']['url']."&scale=8.65&slim=0'";?>"); /* Путь к файлу с исходным  рисунком */
+    display: block; /* Рисунок как блочный элемент */
+    width: 121px;
+    height: 276px;
+    background-position: -8px 0;
+   }
+   a.rollover:hover {
+    background-position: -146px 0;
+   }
+  </style>
 </head>
 <body>
 
@@ -45,20 +59,26 @@ $password = 'password';
         <p>Вводим ник пользователя и получаем иконку и картинку скина</p>
 <?php
 //print_r($profiles);
-if(isset($_POST['username'])) {
-	if($_POST['debug'] == $password) {
-		echo '<p><strong>HASH:</strong> '.$info['SKIN']['hash'];
-		echo ' <strong>ID:</strong> '.$profiles['id'];
-		echo "<br><strong>URL ICON:</strong> http://ely.by/minecraft/skin_buffer/faces/".$info['SKIN']['hash'].".png ";
-		echo "<br><strong>URL SKIN:</strong> http://ely.by/minecraft/skin_buffer/skins/".$info['SKIN']['hash'].".png";
 
+if(isset($_POST['username'])) {
+    echo '<br><strong>ID:</strong> '.$profiles['id']. ' '; echo "<strong>UUID:</strong> ".$uuid;
+	if($_POST['debug'] == $password) {
+    echo ' <strong>ID:</strong> '.$profiles['id'];
+		echo '<p><strong>HASH:</strong> '.$info['SKIN']['url'];
+		echo "<br><strong>URL ICON:</strong> http://ely.by/services/skins-renderer?url=".$info['SKIN']['url']."&scale=18.9&renderFace=1";
+    echo "<br><strong>URL SKIN:</strong> http://ely.by/services/skins-renderer?url=".$info['SKIN']['url']."&scale=8.65&slim=0";
+		echo "<br><strong>URL Textures:</strong> ".$info['SKIN']['url']."";
 }
-	echo "<br><br><img src='http://ely.by/minecraft/skin_buffer/faces/".$info['SKIN']['hash'].".png' style='width: 150px; height: 150px;'>";
-	echo "<br><br><img src='http://ely.by/minecraft/skin_buffer/skins/".$info['SKIN']['hash'].".png'></p>";
+	echo "<br><br><img src='http://ely.by/services/skins-renderer?url=".$info['SKIN']['url']."&scale=18.9&renderFace=1' style='width: 150px; height: 150px;'>";
+  echo "<br><br><p><a class='rollover'></a></p>";
+	echo "<br><br><img src='".$info['SKIN']['url']."'></p>";
 }
 ?>  
-				<form  action="skin.php" method="post">
+
+
+				<form  action="oauth.php" method="post">
 				  <div class="row">
+            
 					<div class="six columns">
 				      <label for="usernametext">Ник пользователя</label>
 				  		<input class="u-full-width" name="username" type="text" placeholder="SanyaFox" id="usernametext">
@@ -74,5 +94,6 @@ if(isset($_POST['username'])) {
 
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
 </body>
 </html>
